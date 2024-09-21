7297a3771c90cbf991d52609f2e5cd07474431a9
tamzi <tamrefrank@gmail.com>
2024-09-22 00:22:18 +0200

    refactor naming

---
+++ rnds/src/main/kotlin/com/recipenews/rnds/RNTheme.kt
@@ -0,0 +1,116 @@
+package com.recipenews.rnds
+
+import android.os.Build
+import androidx.annotation.ChecksSdkIntAtLeast
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.recipenews.rnds.atoms.color.BackgroundTheme
+import com.recipenews.rnds.atoms.color.DarkAndroidColorScheme
+import com.recipenews.rnds.atoms.color.DarkDefaultColorScheme
+import com.recipenews.rnds.atoms.color.DarkGreenGray95
+import com.recipenews.rnds.atoms.color.GradientColors
+import com.recipenews.rnds.atoms.color.LightAndroidColorScheme
+import com.recipenews.rnds.atoms.color.LightDefaultColorScheme
+import com.recipenews.rnds.atoms.color.LocalBackgroundTheme
+import com.recipenews.rnds.atoms.color.LocalGradientColors
+import com.recipenews.rnds.atoms.color.LocalTintTheme
+import com.recipenews.rnds.atoms.color.TintTheme
+import com.recipenews.rnds.atoms.type.RNTypography
+
+
+/**
+ * Light Android gradient colors
+ */
+val LightAndroidGradientColors = GradientColors(container = DarkGreenGray95)
+
+/**
+ * Dark Android gradient colors
+ */
+val DarkAndroidGradientColors = GradientColors(container = Color.Black)
+
+/**
+ * Light Android background theme
+ */
+val LightAndroidBackgroundTheme = BackgroundTheme(color = DarkGreenGray95)
+
+/**
+ * Dark Android background theme
+ */
+val DarkAndroidBackgroundTheme = BackgroundTheme(color = Color.Black)
+
+/**
+ * RecipeNews theme.
+ *
+ * @param darkTheme Whether the theme should use a dark color scheme (follows system by default).
+ * @param androidTheme Whether the theme should use the Android theme color scheme instead of the
+ *        default theme.
+ * @param disableDynamicTheming If `true`, disables the use of dynamic theming, even when it is
+ *        supported. This parameter has no effect if [androidTheme] is `true`.
+ */
+@Composable
+fun RNTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    androidTheme: Boolean = false,
+    disableDynamicTheming: Boolean = true,
+    content: @Composable () -> Unit,
+) {
+    // Color scheme
+    val colorScheme = when {
+        androidTheme -> if (darkTheme) DarkAndroidColorScheme else LightAndroidColorScheme
+        !disableDynamicTheming && supportsDynamicTheming() -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        else -> if (darkTheme) DarkDefaultColorScheme else LightDefaultColorScheme
+    }
+    // Gradient colors
+    val emptyGradientColors = GradientColors(container = colorScheme.surfaceColorAtElevation(2.dp))
+    val defaultGradientColors = GradientColors(
+        top = colorScheme.inverseOnSurface,
+        bottom = colorScheme.primaryContainer,
+        container = colorScheme.surface,
+    )
+    val gradientColors = when {
+        androidTheme -> if (darkTheme) DarkAndroidGradientColors else LightAndroidGradientColors
+        !disableDynamicTheming && supportsDynamicTheming() -> emptyGradientColors
+        else -> defaultGradientColors
+    }
+    // Background theme
+    val defaultBackgroundTheme = BackgroundTheme(
+        color = colorScheme.surface,
+        tonalElevation = 2.dp,
+    )
+    val backgroundTheme = when {
+        androidTheme -> if (darkTheme) DarkAndroidBackgroundTheme else LightAndroidBackgroundTheme
+        else -> defaultBackgroundTheme
+    }
+    val tintTheme = when {
+        androidTheme -> TintTheme()
+        !disableDynamicTheming && supportsDynamicTheming() -> TintTheme(colorScheme.primary)
+        else -> TintTheme()
+    }
+    // Composition locals
+    CompositionLocalProvider(
+        LocalGradientColors provides gradientColors,
+        LocalBackgroundTheme provides backgroundTheme,
+        LocalTintTheme provides tintTheme,
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = RNTypography,
+            content = content,
+        )
+    }
+}
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
+fun supportsDynamicTheming() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
