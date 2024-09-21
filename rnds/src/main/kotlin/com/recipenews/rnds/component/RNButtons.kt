fdeec0b6b2d93df77b0a1a0bc2b1332c30d9a2b6
tamzi <tamrefrank@gmail.com>
2024-09-22 00:48:29 +0200

    Add buttons

--- rnds/src/main/kotlin/com/recipenews/rnds/component/RNButtons.kt
+++ rnds/src/main/kotlin/com/recipenews/rnds/component/RNButtons.kt
@@ -1,2 +1,296 @@
 package com.recipenews.rnds.component
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.recipenews.rnds.RNIcons
+import com.recipenews.rnds.RNTheme
+
+
+/**
+ * RecipeNews filled button with generic content slot. Wraps Material 3 [Button].
+ *
+ * @param onClick Will be called when the user clicks the button.
+ * @param modifier Modifier to be applied to the button.
+ * @param enabled Controls the enabled state of the button. When `false`, this button will not be
+ * clickable and will appear disabled to accessibility services.
+ * @param contentPadding The spacing values to apply internally between the container and the
+ * content.
+ * @param content The button content.
+ */
+@Composable
+fun RNButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    content: @Composable RowScope.() -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.onBackground,
+        ),
+        contentPadding = contentPadding,
+        content = content,
+    )
+}
+
+/**
+ * RecipeNews filled button with text and icon content slots.
+ *
+ * @param onClick Will be called when the user clicks the button.
+ * @param modifier Modifier to be applied to the button.
+ * @param enabled Controls the enabled state of the button. When `false`, this button will not be
+ * clickable and will appear disabled to accessibility services.
+ * @param text The button text label content.
+ * @param leadingIcon The button leading icon content. Pass `null` here for no leading icon.
+ */
+@Composable
+fun RNButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    text: @Composable () -> Unit,
+    leadingIcon: @Composable (() -> Unit)? = null,
+) {
+    RNButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        contentPadding = if (leadingIcon != null) {
+            ButtonDefaults.ButtonWithIconContentPadding
+        } else {
+            ButtonDefaults.ContentPadding
+        },
+    ) {
+        RNButtonContent(
+            text = text,
+            leadingIcon = leadingIcon,
+        )
+    }
+}
+
+/**
+ * RecipeNews outlined button with generic content slot. Wraps Material 3 [OutlinedButton].
+ *
+ * @param onClick Will be called when the user clicks the button.
+ * @param modifier Modifier to be applied to the button.
+ * @param enabled Controls the enabled state of the button. When `false`, this button will not be
+ * clickable and will appear disabled to accessibility services.
+ * @param contentPadding The spacing values to apply internally between the container and the
+ * content.
+ * @param content The button content.
+ */
+@Composable
+fun RNOutlinedButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    content: @Composable RowScope.() -> Unit,
+) {
+    OutlinedButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        colors = ButtonDefaults.outlinedButtonColors(
+            contentColor = MaterialTheme.colorScheme.onBackground,
+        ),
+        border = BorderStroke(
+            width = RNButtonDefaults.OutlinedButtonBorderWidth,
+            color = if (enabled) {
+                MaterialTheme.colorScheme.outline
+            } else {
+                MaterialTheme.colorScheme.onSurface.copy(
+                    alpha = RNButtonDefaults.DISABLED_OUTLINED_BUTTON_BORDER_ALPHA,
+                )
+            },
+        ),
+        contentPadding = contentPadding,
+        content = content,
+    )
+}
+
+/**
+ * RecipeNews outlined button with text and icon content slots.
+ *
+ * @param onClick Will be called when the user clicks the button.
+ * @param modifier Modifier to be applied to the button.
+ * @param enabled Controls the enabled state of the button. When `false`, this button will not be
+ * clickable and will appear disabled to accessibility services.
+ * @param text The button text label content.
+ * @param leadingIcon The button leading icon content. Pass `null` here for no leading icon.
+ */
+@Composable
+fun RNOutlinedButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    text: @Composable () -> Unit,
+    leadingIcon: @Composable (() -> Unit)? = null,
+) {
+    RNOutlinedButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        contentPadding = if (leadingIcon != null) {
+            ButtonDefaults.ButtonWithIconContentPadding
+        } else {
+            ButtonDefaults.ContentPadding
+        },
+    ) {
+        RNButtonContent(
+            text = text,
+            leadingIcon = leadingIcon,
+        )
+    }
+}
+
+/**
+ * RecipeNews text button with generic content slot. Wraps Material 3 [TextButton].
+ *
+ * @param onClick Will be called when the user clicks the button.
+ * @param modifier Modifier to be applied to the button.
+ * @param enabled Controls the enabled state of the button. When `false`, this button will not be
+ * clickable and will appear disabled to accessibility services.
+ * @param content The button content.
+ */
+@Composable
+fun RNTextButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    content: @Composable RowScope.() -> Unit,
+) {
+    TextButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        colors = ButtonDefaults.textButtonColors(
+            contentColor = MaterialTheme.colorScheme.onBackground,
+        ),
+        content = content,
+    )
+}
+
+/**
+ * RecipeNews text button with text and icon content slots.
+ *
+ * @param onClick Will be called when the user clicks the button.
+ * @param modifier Modifier to be applied to the button.
+ * @param enabled Controls the enabled state of the button. When `false`, this button will not be
+ * clickable and will appear disabled to accessibility services.
+ * @param text The button text label content.
+ * @param leadingIcon The button leading icon content. Pass `null` here for no leading icon.
+ */
+@Composable
+fun RNTextButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    text: @Composable () -> Unit,
+    leadingIcon: @Composable (() -> Unit)? = null,
+) {
+    RNTextButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+    ) {
+        RNButtonContent(
+            text = text,
+            leadingIcon = leadingIcon,
+        )
+    }
+}
+
+/**
+ * Internal RecipeNews button content layout for arranging the text label and leading icon.
+ *
+ * @param text The button text label content.
+ * @param leadingIcon The button leading icon content. Default is `null` for no leading icon.Ï
+ */
+@Composable
+private fun RNButtonContent(
+    text: @Composable () -> Unit,
+    leadingIcon: @Composable (() -> Unit)? = null,
+) {
+    if (leadingIcon != null) {
+        Box(Modifier.sizeIn(maxHeight = ButtonDefaults.IconSize)) {
+            leadingIcon()
+        }
+    }
+    Box(
+        Modifier
+            .padding(
+                start = if (leadingIcon != null) {
+                    ButtonDefaults.IconSpacing
+                } else {
+                    0.dp
+                },
+            ),
+    ) {
+        text()
+    }
+}
+
+@ThemePreviews
+@Composable
+fun RNButtonPreview() {
+    RNTheme {
+        RNBackground(modifier = Modifier.size(150.dp, 50.dp)) {
+            RNButton(onClick = {}, text = { Text("Test button") })
+        }
+    }
+}
+
+@ThemePreviews
+@Composable
+fun RNOutlinedButtonPreview() {
+    RNTheme {
+        RNBackground(modifier = Modifier.size(150.dp, 50.dp)) {
+            RNOutlinedButton(onClick = {}, text = { Text("Test button") })
+        }
+    }
+}
+
+@ThemePreviews
+@Composable
+fun RNButtonLeadingIconPreview() {
+    RNTheme {
+        RNBackground(modifier = Modifier.size(150.dp, 50.dp)) {
+            RNButton(
+                onClick = {},
+                text = { Text("Test button") },
+                leadingIcon = { Icon(imageVector = RNIcons.Add, contentDescription = null) },
+            )
+        }
+    }
+}
+
+/**
+ * RecipeNews button default values.
+ */
+object RNButtonDefaults {
+    // OutlinedButton border color doesn't respect disabled state by default
+    const val DISABLED_OUTLINED_BUTTON_BORDER_ALPHA = 0.12f
+
+    // OutlinedButton default border width isn't exposed via ButtonDefaults
+    val OutlinedButtonBorderWidth = 1.dp
+}
