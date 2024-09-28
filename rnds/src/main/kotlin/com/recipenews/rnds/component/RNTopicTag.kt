0a35345a70f441130e66f27be37908593b431945
tamzi <tamrefrank@gmail.com>
2024-09-28 20:28:25 +0200

    RNTopic Tags

---
+++ rnds/src/main/kotlin/com/recipenews/rnds/component/RNTopicTag.kt
@@ -0,0 +1,69 @@
+package com.recipenews.rnds.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.recipenews.rnds.RNTheme
+import com.recipenews.rnds.atoms.color.ThemePreviews
+
+
+@Composable
+fun RNTopicTag(
+    modifier: Modifier = Modifier,
+    followed: Boolean,
+    onClick: () -> Unit,
+    enabled: Boolean = true,
+    text: @Composable () -> Unit,
+) {
+    Box(modifier = modifier) {
+        val containerColor = if (followed) {
+            MaterialTheme.colorScheme.primaryContainer
+        } else {
+            MaterialTheme.colorScheme.surfaceVariant.copy(
+                alpha = RNTagDefaults.UNFOLLOWED_TOPIC_TAG_CONTAINER_ALPHA,
+            )
+        }
+        TextButton(
+            onClick = onClick,
+            enabled = enabled,
+            colors = ButtonDefaults.textButtonColors(
+                containerColor = containerColor,
+                contentColor = contentColorFor(backgroundColor = containerColor),
+                disabledContainerColor = MaterialTheme.colorScheme.onSurface.copy(
+                    alpha = RNTagDefaults.DISABLED_TOPIC_TAG_CONTAINER_ALPHA,
+                ),
+            ),
+        ) {
+            ProvideTextStyle(value = MaterialTheme.typography.labelSmall) {
+                text()
+            }
+        }
+    }
+}
+
+@ThemePreviews
+@Composable
+fun RNTagPreview() {
+    RNTheme {
+        RNTopicTag(followed = true, onClick = {}) {
+            Text("Topic".uppercase())
+        }
+    }
+}
+
+/**
+ * RecipeNews tag default values.
+ */
+object RNTagDefaults {
+    const val UNFOLLOWED_TOPIC_TAG_CONTAINER_ALPHA = 0.5f
+
+    // TODO: File bug
+    // Button disabled container alpha value not exposed by ButtonDefaults
+    const val DISABLED_TOPIC_TAG_CONTAINER_ALPHA = 0.12f
+}
