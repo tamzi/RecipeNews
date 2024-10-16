b91031fad4728527b8030ec12c9fdb9bd5caa213
./root <tamrefrank@gmail.com>
2024-10-16 22:08:35 +0200

    Add buttons to demo screen

--- rndsdemo/src/main/kotlin/com/recipenews/rndsdemo/DemoMainScreen.kt
+++ rndsdemo/src/main/kotlin/com/recipenews/rndsdemo/DemoMainScreen.kt
@@ -2,20 +2,27 @@ package com.recipenews.rndsdemo
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.recipenews.rnds.RNIcons
 import com.recipenews.rnds.RNTheme
 import com.recipenews.rnds.atoms.type.RNTypography
+import com.recipenews.rnds.component.button.RNButton
+import com.recipenews.rnds.component.button.RNOutlinedButton
+import com.recipenews.rnds.component.button.RNTextButton
 
 /**
  * RecipeNews Design System components Demo.
@@ -41,6 +48,87 @@ fun RNDSDemo() {
                         style = RNTypography.displayLarge,
                     )
                 }
+                /** Buttons start here*/
+                item {
+                    Text(
+                        text = "Buttons",
+                        style = RNTypography.headlineMedium,
+                    )
+                }
+                item {
+                    FlowRow(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                        RNButton(onClick = {}) {
+                            Text(text = "Enabled")
+                        }
+                        RNOutlinedButton(onClick = {}) {
+                            Text(text = "Enabled")
+                        }
+                        RNTextButton(onClick = {}) {
+                            Text(text = "Enabled")
+                        }
+                    }
+                }
+                item {
+                    Text(
+                        text = "Disabled Buttons",
+                        style = RNTypography.headlineMedium,
+                    )
+                }
+                item {
+                    FlowRow(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                        RNButton(
+                            onClick = {},
+                            enabled = false,
+                        ) {
+                            Text(text = "Disabled")
+                        }
+                        RNOutlinedButton(
+                            onClick = {},
+                            enabled = false,
+                        ) {
+                            Text(text = "Disabled")
+                        }
+                        RNTextButton(
+                            onClick = {},
+                            enabled = false,
+                        ) {
+                            Text(text = "Disabled")
+                        }
+                    }
+                }
+                item {
+                    Text(
+                        text = "Buttons with Leading Icons",
+                        style = RNTypography.headlineMedium,
+                    )
+                }
+                item {
+                    FlowRow(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                        RNButton(
+                            onClick = {},
+                            text = { Text(text = "Enabled") },
+                            leadingIcon = {
+                                Icon(imageVector = RNIcons.Add, contentDescription = null)
+                            },
+                        )
+                        RNOutlinedButton(
+                            onClick = {},
+                            text = { Text(text = "Enabled") },
+                            leadingIcon = {
+                                Icon(imageVector = RNIcons.Add, contentDescription = null)
+                            },
+                        )
+                        RNTextButton(
+                            onClick = {},
+                            text = { Text(text = "Enabled") },
+                            leadingIcon = {
+                                Icon(imageVector = RNIcons.Add, contentDescription = null)
+                            },
+                        )
+                    }
+                }
+
+
             }
         }
     }
