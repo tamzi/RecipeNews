d434753b6a55ed9c7a349328009f28435a6851ca
./root <tamrefrank@gmail.com>
2024-10-31 21:26:22 +0100

    Start RN info card

---
+++ rnds/src/main/kotlin/com/recipenews/rnds/utility/RNDimensions.kt
@@ -0,0 +1,18 @@
+package com.recipenews.rnds.utility
+
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+
+data class RNDimensions(
+    val paddingNone: Dp = 0.dp,
+    val paddingSmall: Dp = 4.dp,
+    val paddingMedium: Dp = 8.dp,
+    val paddingLarge: Dp = 12.dp,
+    val paddingXL: Dp = 16.dp,
+    val paddingXXL: Dp = 24.dp,
+    val paddingXXXL: Dp = 36.dp
+)
+
+internal val LocalDimensions = staticCompositionLocalOf { RNDimensions() }
